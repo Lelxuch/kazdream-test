4ab720aeaad0620d1ecb06bf5421de682e4e46f3
Sanzhar Nakyp <nakyp.sanzhar@gmail.com>
2023-01-08 03:11:41 +0600

    feat: tabs added

--- src/app/sections/products/pages/default-page/default-page.component.ts
+++ src/app/sections/products/pages/default-page/default-page.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+import {ProductsService} from 'src/app/core/services/products.service';
 
 @Component({
   selector: 'app-default-page',
@@ -7,9 +9,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DefaultPageComponent implements OnInit {
 
-  constructor() { }
+  inputFilter: string;
+  products: any = [];
+  filteredProducts: any = [];
+
+  index = 0;
+  tabs: string[] = []
+
+  constructor(
+    private productsService: ProductsService
+  ) { }
 
   ngOnInit(): void {
+    this.productsService.getProducts()
+      .subscribe(res => {
+        this.products = res;
+      })
+  }
+
+  applyFilter() {
+    if (!this.inputFilter) {
+      this.filteredProducts = [];
+      return;
+    }
+    this.filteredProducts = this.products.filter((product: any, count: number) => {
+      if (count < 10) {
+        count++;
+        return product.title.toLowerCase().includes(this.inputFilter.toLowerCase());
+      }
+      return false;
+    });
+  }
+
+  openProduct(item: any) {
+    console.log(item);
+  }
+
+  closeTab({ index }: { index: number }): void {
+    this.tabs.splice(index, 1);
+  }
+
+  newTab(): void {
+    this.tabs.push('New Tab');
+    this.index = this.tabs.length - 1;
   }
 
 }
--- src/app/sections/products/products.module.ts
+++ src/app/sections/products/products.module.ts
@@ -1,6 +1,12 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
+import {FormsModule} from '@angular/forms';
+
+import {NzInputModule} from 'ng-zorro-antd/input';
+import {NzListModule} from 'ng-zorro-antd/list';
+import {NzTabsModule} from 'ng-zorro-antd/tabs';
+import {NzButtonModule} from 'ng-zorro-antd/button';
 
 import {DefaultPageComponent} from './pages/default-page/default-page.component';
 
@@ -15,6 +21,11 @@ const routes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
+    FormsModule,
+    NzInputModule,
+    NzListModule,
+    NzTabsModule,
+    NzButtonModule
   ]
 })
 export class ProductsModule { }
